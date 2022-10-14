60b0715f8b89c88708e3b9e1f570625f162f2904
21bq1a4212vvit <114382610+21bq1a4212vvit@users.noreply.github.com>
2022-10-14 09:26:35 +0530

    Add files via upload

---
+++ checkingOfVowels.py
@@ -0,0 +1,5 @@
+s=input().lower()
+v='aeiou'
+for  k in s:s.replace(k,'')
+print("Yes the given statement consists o all voewls " if v=='' else 'No.. The given statement does not consists of all voewls ')
+
